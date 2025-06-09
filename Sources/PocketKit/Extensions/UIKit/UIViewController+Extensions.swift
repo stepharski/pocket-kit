3ac4be87a960091824aa771106b57167446de658
Stepan Kukharskyi <stepharski@gmail.com>
2025-06-09 14:54:52 -0400

    Add UIViewController extensions for presenting alert and dismissKeyboard

---
+++ Sources/PocketKit/Extensions/UIKit/UIViewController+Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  PocketKit
+//
+//  Created by Stepan Kukharskyi on 6/9/25.
+//
+
+import UIKit
+
+// MARK: - UIViewController + Extensions
+extension UIViewController {
+    // MARK: Alert
+    var isPresentingAlert: Bool {
+        return presentedViewController is UIAlertController
+    }
+
+    func showAlert(title: String = "Oops!", message: String) {
+        guard !isPresentingAlert else { return }
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Got it", style: .default))
+        present(alertVC, animated: true)
+    }
+
+    // MARK: Keyboard
+    func dismissKeyboardOnTouchOutside() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
