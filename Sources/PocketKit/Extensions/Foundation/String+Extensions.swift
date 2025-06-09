e5f09a42c602c58e13ef72be803494ce8d1c6f71
Stepan Kukharskyi <stepharski@gmail.com>
2025-06-09 13:20:18 -0400

    Update String extension

--- Sources/PocketKit/Extensions/Foundation/String+Extensions.swift
+++ Sources/PocketKit/Extensions/Foundation/String+Extensions.swift
@@ -8,8 +8,23 @@
 import Foundation
 
 public extension String {
-    static let empty = ""
-    static let comma = ","
-    static let newLine = "\n"
-    static let whitespace = " "
+    static let empty: String = ""
+    static let space: String = " "
+    static let comma: String = ","
+    static let period: String = "."
+    static let colon: String = ":"
+    static let semicolon: String = ";"
+    static let dash: String = "-"
+    static let underscore: String = "_"
+    static let newLine: String = "\n"
+    static let tab: String = "\t"
+    static let slash: String = "/"
+    static let backslash: String = "\\"
+    static let questionMark: String = "?"
+    static let exclamationMark: String = "!"
+    static let ampersand: String = "&"
+    static let at: String = "@"
+    static let hash: String = "#"
+    static let dollar: String = "$"
+    static let percent: String = "%"
 }
